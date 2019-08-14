d2a5bceec0e11a0bd1a81104b0c77cde7ac40b50
Or Cherfas <or@run.ai>
2019-08-14 20:26:11 +0300

    removed redudent line and added production run

--- proxy/main.ts
+++ proxy/main.ts
@@ -8,20 +8,25 @@ import proxy from 'http-proxy-middleware';
 const app = express();
 
 const secret = process.env.SECRET;
-console.log(secret);
 
-let ipAddress = '10.100.102.2';
+let ipAddress;
 
 app.use('/transmission', (req, res, next) => {
-    return proxy({
-        target: `http://${ipAddress}:9091/transmission/rpc`,
-        logLevel: "silent"
-    })(req, res, next);
+    if(ipAddress){
+        return proxy({
+            target: `http://${ipAddress}:9091/transmission/rpc`,
+            logLevel: "silent"
+        })(req, res, next);
+    }
+    else {
+        res.sendStatus(500);
+    }
 });
 
 app.post('/transmission-url', expressJWT({secret: secret}), bodyParser.json(), (req, res) => {
     const address = get(req, 'body.address', ipAddress);
     ipAddress = address;
+    console.log(`updated address to ${ipAddress}`);
     res.sendStatus(201);
 })
 
